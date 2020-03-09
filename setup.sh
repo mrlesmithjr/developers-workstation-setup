2625259fef9dde1507abe95a655b5535fa96fa43
Larry Smith Jr <mrlesmithjr@gmail.com>
2020-03-09 10:39:33 -0400

    Added more logic to ensure existing dotfiles
    - This new logic will ensure that our dotfiles are in a good state and
    pointing to the correct location, etc.
    - Also captures the current branch, stashes any changes, switches to
    master if not already in master. Executes dotfiles setup, then switches
    to the original branch if not in master. The pops any stashed changes.

--- setup.sh
+++ setup.sh
@@ -7,21 +7,45 @@ BUILD_DIR="$PWD"
 
 # Define where dotfiles repo exists
 DOTFILES_DIR="$HOME/.dotfiles"
+# Define dotfiles remote repo
+DOTFILES_REPO="https://github.com/mrlesmithjr/dotfiles.git"
 
 # First we need to check and see if the original dotfiles repo structure is in
 # place. If it is, back it up and clone the new structure and setup.
 if [ ! -d "$DOTFILES_DIR" ]; then
-  git clone https://github.com/mrlesmithjr/dotfiles-new "$DOTFILES_DIR" --recurse-submodules
+  git clone "$DOTFILES_REPO" "$DOTFILES_DIR" --recurse-submodules
+  # shellcheck source=/dev/null
   source "$DOTFILES_DIR/setup.sh"
   git config --global user.name ""
   git config --global user.email ""
 else
   if [ -f "$DOTFILES_DIR/install/setup.sh" ]; then
     mv "$DOTFILES_DIR" "$DOTFILES_DIR.orig"
-    git clone https://github.com/mrlesmithjr/dotfiles-new "$DOTFILES_DIR" --recurse-submodules
+    git clone "$DOTFILES_REPO" "$DOTFILES_DIR" --recurse-submodules
+    # shellcheck source=/dev/null
     source "$DOTFILES_DIR/setup.sh"
     git config --global user.name ""
     git config --global user.email ""
+  else
+    cd "$DOTFILES_DIR"
+    git stash
+    git remote remove origin
+    git remote add origin "$DOTFILES_REPO"
+    CURRENT_BRANCH="$(git rev-parse --abbrev-ref HEAD)"
+    if [[ "$CURRENT_BRANCH" != "master" ]]; then
+      git checkout master
+    fi
+    git fetch
+    git branch --set-upstream-to=origin/master master
+    git pull
+    git submodule update --remote --recursive
+    # shellcheck source=/dev/null
+    source "$DOTFILES_DIR/setup.sh"
+    cd "$DOTFILES_DIR"
+    if [[ "$CURRENT_BRANCH" != "master" ]]; then
+      git checkout "$CURRENT_BRANCH"
+    fi
+    git stash pop
   fi
 fi
 
@@ -95,6 +119,7 @@ if [[ $(uname) == "Darwin" ]]; then
   if [ -d "$BUILD_DIR/tools/time_machine_exclusions" ]; then
     if [ -f "$BUILD_DIR/tools/time_machine_exclusions/install.sh" ]; then
       cd "$BUILD_DIR/tools/time_machine_exclusions"
+      # shellcheck source=/dev/null
       source install.sh
     fi
   fi
