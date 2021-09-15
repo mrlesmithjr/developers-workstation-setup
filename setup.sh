393b28d32c03b87e83532f078712b047565d0b8d
Larry Smith Jr <mrlesmithjr@gmail.com>
2021-09-15 10:53:05 -0400

    excluding Python miniforge versions

--- setup.sh
+++ setup.sh
@@ -55,7 +55,7 @@ if [ ! -d "$PYENV_ROOT" ]; then
 	git clone https://github.com/pyenv/pyenv.git "$PYENV_ROOT"
 	git clone https://github.com/pyenv/pyenv-virtualenv.git "$PYENV_ROOT/plugins/pyenv-virtualenv"
 	export PATH="$PYENV_ROOT/bin:$PATH"
-	DEFAULT_PYTHON_VERSION=$(pyenv install --list | grep -v - | grep -v b | grep -v rc | tail -1 | awk '{ print $1 }')
+	DEFAULT_PYTHON_VERSION=$(pyenv install --list | grep -v - | grep -v b | grep -v mini | grep -v rc | tail -1 | awk '{ print $1 }')
 	pyenv install "$DEFAULT_PYTHON_VERSION"
 	pyenv global "$DEFAULT_PYTHON_VERSION"
 	eval "$(pyenv init --path)"
